4fa46ca5418adeb8bd99363f1e0e049114d65030
Антонина Леонидовна Зинкевич <Zinkevich@MBSP.ru>
2020-10-15 16:48:06 +0300

    units error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
+    let units=['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teens=['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens=['twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+  let numberArray=String(number).split('');
+  let result='';
+  if (numberArray.length===3){
+      result=result+units[numberArray[0]] +' hundred ';
+      if(numberArray[1]===0 && numberArray[2]===0) return result.trim();
+  }
+  if (numberArray[1]===1){
+    result+=teens[numberArray[numberArray.length-1]];
+    return result.trim();
+  }
+  if(numberArray[1]>1){
+      result=result+tens[numberArray[1]-2];
+      if(numberArray[numberArray.length-1]===0) return result.trim();
+  }
+  
+    result+=units[numberArray[numberArray.length]];
+
+    return result.trim();
+    
   
 }
