8b95326c36bd42a63d4dc8aa61b6870e06e9e44a
tone4ka <toniamik@gmail.com>
2020-10-15 22:39:19 +0300

    yyyypppphhh

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 module.exports = function toReadable (number) {
-    let units=['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let units=['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let teens=['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let tens=['twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
   let numberArray=String(number).split('');
+  let length=numberArray.length;
   let result='';
-  if (numberArray.length===3){
+  if(number===0)return 'zero';
+  if (length===3){
       result=result+units[numberArray[0]] +' hundred ';
-      if(numberArray[1]===0 && numberArray[2]===0) return result.trim();
+      if(numberArray[1]===0 && numberArray[2]===0){
+        return result.trim();
+      } 
   }
-  if (numberArray[1]===1){
-    result+=teens[numberArray[numberArray.length-1]];
+  if (numberArray[length-2]&&numberArray[length-2]===1){
+    result+=teens[numberArray[length-1]];
     return result.trim();
   }
-  if(numberArray[1]>1){
+  if(numberArray[length-2]>1){
       result=result+tens[numberArray[1]-2];
-      if(numberArray[numberArray.length-1]===0) return result.trim();
+      if(numberArray[length-1]===0){
+        return result.trim();
+      } 
   }
-  
-    result+=units[numberArray[numberArray.length]];
-
+  if(numberArray[length-2]!=1){
+    result+=units[numberArray[length-1]-1];
     return result.trim();
+  }
     
-  
+
+    // return result.trim();
+    
+
 }
