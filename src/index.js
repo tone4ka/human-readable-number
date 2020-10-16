4867cc7ba99c0d467e597bcc6909d34b6011a9b6
tone4ka <toniamik@gmail.com>
2020-10-16 22:23:15 +0300

    feat:function correction

--- src/index.js
+++ src/index.js
@@ -5,30 +5,27 @@ module.exports = function toReadable (number) {
   let numberArray=String(number).split('');
   let length=numberArray.length;
   let result='';
-  if(number===0)return 'zero';
-  if (length===3){
+  if(number==0)return 'zero';
+  if (length==3){
       result=result+units[numberArray[0]-1] +' hundred ';
-      if(numberArray[1]===0 && numberArray[2]===0){
+      if(numberArray[1]==0 && numberArray[2]==0){
         return result.trim();
       } 
   }
-  if (numberArray[length-2]&&numberArray[length-2]===1){
+  if (numberArray[length-2]&&numberArray[length-2]==1){
     result+=teens[numberArray[length-1]];
     return result.trim();
   }
   if(numberArray[length-2]>1){
-      result=result+tens[numberArray[1]-2];
-      if(numberArray[length-1]===0){
-        return result.trim();
-      } 
+    result=result+tens[numberArray[length-2]-2];
+    if(numberArray[length-1]==0){
+      return result.trim();
+    } 
   }
   if(numberArray[length-2]!=1){
     result+=units[numberArray[length-1]-1];
     return result.trim();
   }
-    
-
-    // return result.trim();
-    
+      
 
 }
